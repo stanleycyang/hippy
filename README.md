bbe82c87b5093ef6f7c41103b34e485e22b83ad5
stanleycyang <stanley@stanleycyang.com>
2015-09-05 13:25:30 -0700

    Modified README to include links

---
+++ README.md
@@ -0,0 +1,36 @@
+#Hippy
+
+## Installation
+
+```sh
+$ npm install -g hippy
+```
+
+## Quick start
+
+Create the app:
+
+```bash
+$ hippy demo_app
+```
+
+Install dependencies:
+
+```bash
+$ npm install
+```
+
+Start
+
+```bash
+$ hippy start
+```
+
+## License
+[MIT](LICENSE)
+
+[npm-image]: https://img.shields.io/npm/v/hippy.svg
+[npm-url]: https://www.npmjs.com/package/hippy
+[downloads-image]: https://img.shields.io/npm/dm/hippy.svg
+[download-url]: https://www.npmjs.com/package/hippy
+
--- test/cmd.js
+++ test/cmd.js
@@ -10,6 +10,28 @@ import rimraf from 'rimraf'; // rm -rf for NodeJS
 const binPath = path.resolve(__dirname, '../bin/express');
 const tempDir = path.resolve(__dirname, '../temp');
 
+describe('express(1)', () => {
+  mocha.before( done => {
+    this.timeout(30000);
+    cleanup(done);
+  });
+
+  mocha.after( done => {
+    this.timeout(30000);
+    cleanup(done);
+  });
+
+  describe('(no args)', () => {
+    let dir, files, output;
+
+    mocha.before( done => {
+    });
+  });
+
+});
+
+
+
 /*
  * Clean up the directory after testing
  *
@@ -26,6 +48,40 @@ function cleanup(dir, callback) {
   rimraf(tempDir, error => {
     callback(error);
   });
+}
+
+function createEnvironment(callback) {
+  let num = process.pid + Math.random();
+  let dir = path.join(tempDir, ('app-' + num));
+
+  mkdirp(dir, function ondir(error) {
+    if(error) return callback(error);
+    callback(null, dir);
+  });
+}
+
+function parseCreatedFiles(output, dir) {
+  let files = [];
+  let lines = output.split(/[\r\n]+/);
+  let match;
+
+  for (let i = 0; i < lines.length; i++) {
+    if ((match = /create.*?: (.*)$/.exec(lines[i]))) {
+      let files = match[1];
+
+      if (dir) {
+        file = path.resolve(dir, file);
+        file = path.relative(dir, file);
+      }
+
+      file = file.replace(/\\/g, '/');
+      files.push(file);
+    }
+  }
+  return files;
+}
+
+function run(dir, args, callback) {
 }
 
 function npmInstall(dir, callback) {
